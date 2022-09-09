163482db80fecd0768b54dd67e8e1f25f03765a3
col-kol <collinkolars1@gmail.com>
2022-09-09 18:51:19 -0500

    .

--- json-alphabetizer/src/lib.rs
+++ json-alphabetizer/src/lib.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use std::error::Error;
+use serde_json;
 
 pub fn run(filename: &str) -> Result<(), Box<dyn Error>> {
     // print file name
@@ -8,6 +9,8 @@ pub fn run(filename: &str) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
     //println!("\n{}", contents);
+    alphabetize(contents);
+
     Ok(())
 }
 
@@ -18,4 +21,7 @@ pub fn parse_args(args: &[String]) -> &str {
     filename
 }
 
-
+pub fn alphabetize(data) {
+    let people: Vec<String> = serde_json::from_str(data)?;
+    println!("\n{}", people);
+}
