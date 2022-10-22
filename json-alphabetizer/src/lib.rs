e8178916040db4dae276ea8ee855db160a0f3b70
col-kol <collinkolars1@gmail.com>
2022-10-22 13:22:24 -0500

    read json as untyped string

--- json-alphabetizer/src/lib.rs
+++ json-alphabetizer/src/lib.rs
@@ -1,18 +1,22 @@
 use std::fs;
-use std::error::Error;
+use serde_json::{Result, Value};
 
-pub fn run(filename: &str) -> Result<(), Box<dyn Error>> {
+
+pub fn run(filename: &str) -> Result<()> {
     // print file name
     println!("Filename: {:?}", filename);
     // read in the file
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
-    //println!("\n{}", contents);
+    
+    println!("contents:\n{}", contents);
+    let json: Value = serde_json::from_str(&contents)
+        .expect("JSON is malformed");
+    println!("Value:\n{}", json);
     Ok(())
 }
 
 pub fn parse_args(args: &[String]) -> &str {
-    // TODO: convert this from 1 to N json files
     let filename = &args[1];
     
     filename
